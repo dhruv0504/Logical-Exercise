2e50723a09b7c34d5990617af588df20aaabb18c
Dhruvkumar Tilva <94628400+dhruv0504@users.noreply.github.com>
2023-03-03 15:21:52 +0530

    Update and rename day1prbl2easy.py to Merge Two Sorted Lists.py

--- Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists.py
@@ -1,11 +1,11 @@
 """"
 link: https://leetcode.com/problems/merge-two-sorted-lists/ 
-ans-link: https://colab.research.google.com/drive/1YENAlJo_Goe4CY0g6XV1nX6muGctyI1Z
-You are given the heads of two sorted linked lists list1 and list2.
+
+You are given the heads of two sorted lists list1 and list2.
 
 Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
-Return the head of the merged linked list.
+Return the head of the merged list.
 
  
 
@@ -27,7 +27,6 @@ Output: [0]
 Constraints:
 
 The number of nodes in both lists is in the range [0, 50].
--100 <= Node.val <= 100
 Both list1 and list2 are sorted in non-decreasing order.
 """
 
