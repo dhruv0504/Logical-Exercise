897144ca0806f2bb70ef901a977320714dec18c3
Dhruvkumar Tilva <94628400+dhruv0504@users.noreply.github.com>
2023-03-03 15:20:05 +0530

    Update and rename day1prbl1easy.py to Search Insert Position.py

--- Search Insert Position.py
+++ Search Insert Position.py
@@ -1,6 +1,6 @@
 """
 link: https://leetcode.com/problems/search-insert-position/
-ans-link: https://colab.research.google.com/drive/1d7O8SItnbkU8VpweAVG_4__nFK2JgW90
+
 
 Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
