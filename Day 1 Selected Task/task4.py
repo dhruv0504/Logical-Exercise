ca4b78c4a470826710dba294d9ccfe2a3ab145f0
Dhruvkumar Tilva <94628400+dhruv0504@users.noreply.github.com>
2023-03-16 17:42:52 +0530

    Add files via upload

---
+++ Day 1 Selected Task/task4.py
@@ -0,0 +1,22 @@
+"""
+4. gfg(e)
+https://practice.geeksforgeeks.org/problems/stock-buy-and-sell-1587115621/1?page=1&curated[]=1&sortBy=submissions
+
+status: submitted
+"""
+def maxProfit(prices,N):
+        left = 0 #Buy
+        right = 1 #Sell
+        max_profit = 0
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left] #our current Profit
+            if prices[left] < prices[right]:
+                max_profit = max(currentProfit,max_profit)
+            else:
+                left = right
+            right += 1
+        if max_profit > 0:
+             return 1
+        else: return 0
+
+print(maxProfit([100],52))
